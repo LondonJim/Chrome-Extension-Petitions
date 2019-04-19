6cb424802b47b694035733224f680f43dbe56edc
James Sutherland <jrsutherland78@googlemail.com>
2019-04-19 14:17:27 +0100

    add content.js, sticky top bar

--- content.js
+++ content.js
@@ -1,1 +1,21 @@
+let toolbarHeight = 50;
 
+let div = document.createElement("div");
+div.id = "myToolbar";
+div.textContent = "Random Petitions";
+
+let st = div.style;
+st.padding = "5px";
+st.display = "block";
+st.top = "0px";
+st.left = "0px";
+st.width = "100%";
+st.height = toolbarHeight + "px";
+st.background = "#008800";
+st.color = "#FFFFFF";
+st.fontSize = "24px";
+st.fontFamily = "Helvetica Neue"
+st.position = "fixed";
+
+document.body.style.webkitTransform = "translateY(" + toolbarHeight + "px)";
+document.documentElement.appendChild(div);
