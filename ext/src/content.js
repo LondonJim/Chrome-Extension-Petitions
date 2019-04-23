36fe5ac36d1f6b9ceea4f30cb05ca6081b8ccf36
James Sutherland <jrsutherland78@googlemail.com>
2019-04-23 17:21:31 +0100

    create .DisplayPetitions

--- ext/src/content.js
+++ ext/src/content.js
@@ -1,52 +1,63 @@
-let promises = []
-let petitions = []
-let randomPetition
-let randomPetitionId
+class DisplayPetitions {
 
-displayRandomizePetition = () => {
-  randomPetition = petitions[Math.floor(Math.random() * petitions.length)]
-  randomPetitionId = randomPetition.id
-  document.getElementById("heading").innerText = randomPetition.attributes.action
-  document.getElementById("petition-count").innerText = numberWithCommas(randomPetition.attributes.signature_count) + " signatures so far!"
-}
-
-linkToPetition = () => {
-  let url = `https://petition.parliament.uk/petitions/${randomPetitionId}`
-  chrome.tabs.update({active: true, url: url})
-}
-
-numberWithCommas = (x) => {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-returnResults = () => {
-  petitions = petitions.flat()
-  displayRandomizePetition()
-}
-
-addToPetitions = (jsonResults) => {
-  petitions.push(jsonResults)
-}
-
-getPetitions = () => {
-  for (let i = 1; i <= 40; i++) {
-    let promise = fetch(`https://petition.parliament.uk/petitions.json?page=${i}&state=open`)
-            .then(results => { return results.json()})
-            .then (jsonResults => {
-                addToPetitions(jsonResults.data)
-            })
-    promises.push(promise)
+  constructor () {
+    this.promises = []
+    this.petitions = []
+    this.randomPetition = []
+    this.randomPetitionId = 0
   }
+
+  execute = () => {
+    this.getPetitions()
+    Promise.all(this.promises)
+      .then(values => {
+        this.returnResults()
+        document.getElementById("change-link")
+          .addEventListener("click", this.displayRandomizePetition)
+        document.getElementById("view-link")
+          .addEventListener("click", this.linkToPetition)
+      })
+  }
+
+  displayRandomizePetition = () => {
+    this.randomPetition = this.petitions[Math.floor(Math.random() * this.petitions.length)]
+    this.randomPetitionId = this.randomPetition.id
+    document.getElementById("heading")
+      .innerText = this.randomPetition.attributes.action
+    document.getElementById("petition-count")
+      .innerText = this.numberWithCommas(this.randomPetition.attributes.signature_count) + " signatures so far!"
+  }
+
+  linkToPetition = () => {
+    let url = `https://petition.parliament.uk/petitions/${this.randomPetitionId}`
+    chrome.tabs.update({active: true, url: url})
+  }
+
+  numberWithCommas = (x) => {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  returnResults = () => {
+    this.petitions = this.petitions.flat()
+    this.displayRandomizePetition()
+  }
+
+  addToPetitions = (jsonResults) => {
+    this.petitions.push(jsonResults)
+  }
+
+  getPetitions = () => {
+    for (let i = 1; i <= 40; i++) {
+      let promise = fetch(`https://petition.parliament.uk/petitions.json?page=${i}&state=open`)
+              .then(results => { return results.json()})
+              .then (jsonResults => {
+                  this.addToPetitions(jsonResults.data)
+              })
+      this.promises.push(promise)
+    }
+  }
+
 }
 
-execute = () => {
-  getPetitions()
-  Promise.all(promises)
-    .then(values => {
-      returnResults()
-      document.getElementById("change-link").addEventListener("click", displayRandomizePetition)
-      document.getElementById("view-link").addEventListener("click", linkToPetition)
-    })
-}
-
-execute()
+display = new DisplayPetitions()
+display.execute()
