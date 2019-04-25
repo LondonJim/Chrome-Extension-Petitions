f0d2006897e8f71008235b49f0062a83bb71b39a
James Sutherland <jrsutherland78@googlemail.com>
2019-04-25 22:23:53 +0100

    add more options for h1 tag text query selector

--- content.js
+++ content.js
@@ -2,10 +2,14 @@ chrome.runtime.onMessage.addListener(
       function(request, sender, sendResponse) {
         if (request.message === "start") {
           headlineString = document.querySelectorAll('h1')[0].textContent
-          if (headlineString.length < 3) {
-            headlineString = document.querySelectorAll('h2')[0].textContent
+          if (headlineString.length < 5) {
+            headlineString = document.querySelectorAll('h1')[1].textContent
+            if (headlineString.length < 5) {
+              headlineString = document.querySelectorAll('h2')[0].textContent
+            }
           }
           sendHeadline(headlineString)
+          console.log(headlineString.length)
         }
         return true;
       }
