aec567e4dc8f8e9e3c514a80d29bb86d1cbb5a4d
James Sutherland <jrsutherland78@googlemail.com>
2019-04-25 22:12:36 +0100

    revert to h2 tag headlines if h1 tag headlines are not present

--- content.js
+++ content.js
@@ -1,7 +1,10 @@
 chrome.runtime.onMessage.addListener(
       function(request, sender, sendResponse) {
-        if( request.message === "start" ) {
+        if (request.message === "start") {
           headlineString = document.querySelectorAll('h1')[0].textContent
+          if (headlineString.length < 3) {
+            headlineString = document.querySelectorAll('h2')[0].textContent
+          }
           sendHeadline(headlineString)
         }
         return true;
