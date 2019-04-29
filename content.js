dc37e27db387445252933dba6e0624f6392bd653
James Sutherland <jrsutherland78@googlemail.com>
2019-04-29 11:04:37 +0100

    catch err if no heading tags are present

--- content.js
+++ content.js
@@ -1,18 +1,27 @@
 chrome.runtime.onMessage.addListener(
       function(request, sender, sendResponse) {
         if (request.message === "start") {
-          headlineString = document.querySelectorAll('h1')[0].textContent
-          if (headlineString.length < 5) {
-            headlineString = document.querySelectorAll('h1')[1].textContent
-            if (headlineString.length < 5) {
-              headlineString = document.querySelectorAll('h2')[0].textContent
-            }
-          }
+          headlineString = getHeaderTag()
           sendHeadline(headlineString)
         }
         return true;
       }
     )
+
+getHeaderTag = () => {
+  try {
+    headlineString = document.querySelectorAll('h1')[0].textContent
+    if (headlineString.length < 5) {
+      headlineString = document.querySelectorAll('h1')[1].textContent
+      if (headlineString.length < 5) {
+        headlineString = document.querySelectorAll('h2')[0].textContent
+      }
+    }
+  } catch (err) {
+    return ""
+  }
+  return headlineString
+}
 
 sendHeadline = (headlineString) => {
   chrome.runtime.sendMessage({headline: headlineString})
