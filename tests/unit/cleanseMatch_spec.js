88910b8e4dda8575734e6bc7719d788ad7be78b1
James Sutherland <jrsutherland78@googlemail.com>
2019-05-01 11:08:39 +0100

    add .CleanseMatch #execute unit test

---
+++ tests/unit/cleanseMatch_spec.js
@@ -0,0 +1,29 @@
+let rewire = require('rewire')
+
+let cleanseMatch = rewire('../../popup/src/cleanseMatch.js')
+
+CleanseMatch = cleanseMatch.__get__('CleanseMatch')
+
+describe('.CleanseMatch', () => {
+
+  describe('#execute', () => {
+
+    let mockCleanseString = (stringToCleanse) => {
+      return stringToCleanse
+    }
+
+    let mockCosineSimilarity = (stringToCleanse) => {
+      return 1
+    }
+
+    it('should return join strings and return a float', () => {
+      cleanse = new CleanseMatch(mockCleanseString, mockCosineSimilarity)
+      let petitions = [{'attributes': {'action': '00', 'background': '01', 'additional_details': '02'}},
+                       {'attributes': {'action': '10', 'background': '11', 'additional_details': '12'}},
+                       {'attributes': {'action': '20', 'background': '21', 'additional_details': '22'}},
+                       {'attributes': {'action': '30', 'background': '31', 'additional_details': '32'}}]
+      let headline = 'this is a test'
+      expect(cleanse.execute(petitions, headline)).toEqual(0)
+    })
+  })
+})
